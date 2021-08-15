18afde1c45b633a21e3b8b8ba59b8107a7472a90
Mike Gering <git@mfg.ferree-gering.com>
2021-08-15 10:21:27 -0400

    broken

--- docker_mon.py
+++ docker_mon.py
@@ -11,11 +11,14 @@ import subprocess
 import sys
 
 HOST="alphocker.dawson"
-CONTAINER="mfgtransovpnproxy"
+#CONTAINER="mfgtransovpnproxy"
+CONTAINER="digikam"
 # Ports are handled in ~/.ssh/config since we use OpenSSH
 #COMMAND = f"docker container inspect {CONTAINER}"
 #docker ps --filter "name=mfgtransovpnproxy" --format "{{.ID}}: {{.Status}}"
-COMMAND = 'docker ps --all --filter "name=mfgtransovpnproxy" --format "{{.Names}}\t{{.ID}}\t{{.Status}}"'
+TEMPLATE = '{{.Names}}\t{{.ID}}\t{{.Status}}'
+#COMMAND = 'docker ps --all --filter "name=mfgtransovpnproxy" --format "{{.Names}}\t{{.ID}}\t{{.Status}}"'
+COMMAND = f'docker ps --all --filter "name={CONTAINER}" --format "{TEMPLATE}"'
 
 ssh = subprocess.Popen(["ssh", HOST, COMMAND],
                        shell=False, 
@@ -27,13 +30,18 @@ if result == []:
     print(f"ERROR: {error}")
 else:
     health_pattern = r"[^\(]+\((.*?)\)"
+    state_pattern = r"^(.*?)\s+"
     for line in result:
         (names, id, status) = line.decode("utf-8").split("\t")
-        m = re.search(health_pattern, status)
-        if m:
-            health = m.group(1)
-        else:
-            pass # no health!
+        if names == CONTAINER:
+            m = re.search(state_pattern, status)
+            state = None
+            if m
+            m = re.search(health_pattern, status)
+            if m:
+                health = m.group(1)
+            else:
+                pass # no health!
         print(line.decode("utf-8"))
     output = [x.decode("utf-8") for x in result]
     print(output)
